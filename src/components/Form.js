e98ce2b392fb39e9b39a40a1f19132df406d720c
joedip16 <joedippolito16@gmail.com>
2021-01-08 09:27:47 -0500

    added filter functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,48 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 
 
-class Form extends Component {
-  // Setting the component's initial state
-  state = {
-    firstName: "",
-    lastName: ""
-  };
-
-  handleInputChange = event => {
-    // Getting the value and name of the input which triggered the change
-    const { name, value } = event.target;
-
-    // Updating the input's state
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleFormSubmit = event => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    event.preventDefault();
-  };
-
-  render() {
-    // Notice how each input has a `value`, `name`, and `onChange` prop
+export default function Form(props) {
     return (
       <div>
-        <p>
-          Hello {this.state.firstName} {this.state.lastName}
-        </p>
-        <form className="form">
+        <form className="form ">
           <input
-            value={this.state.firstName}
-            name="firstName"
-            onChange={this.handleInputChange}
+            value={props.search}
+            name="search"
+            onChange={props.handleInputChange}
             type="text"
-            placeholder=""
+            placeholder="Search for Employee"
           />        
-          <button onClick={this.handleFormSubmit}>Search</button>
         </form>
       </div>
     );
-  }
 }
 
-export default Form;
--- src/components/Table.js
+++ src/components/Table.js
@@ -8,6 +8,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import axios from "axios"
+import Form from "./Form"
 
 const styles = () =>
     createStyles({
@@ -17,7 +18,7 @@ const styles = () =>
     });
 
 class BasicTable extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             users: [],
@@ -25,38 +26,56 @@ class BasicTable extends React.Component {
         }
 
     }
-    componentDidMount(){
+    componentDidMount() {
         axios.get("https://randomuser.me/api/?results=25&nat=us").then(res => {
             console.log(res.data)
-            this.setState({users: res.data.results})
+            this.setState({ users: res.data.results })
         })
     }
+
+    handleInputChange = event => {
+        // Getting the value and name of the input which triggered the change
+        let { name, value } = event.target;
+
+        // Updating the input's state
+        this.setState({
+            [name]: value
+        });
+    };
+
+    sortByName(){
+        this.setState({
+            users: this.state.users.sort((a, b) => a.name.last > b.name.last ? 1:-1)
+        })
+    }
+
     render() {
         const { classes } = this.props;
         return (
             <TableContainer component={Paper}>
+                <Form search={this.state.search} handleInputChange={this.handleInputChange} />
                 <Table className={classes.table} aria-label="simple table">
                     <TableHead>
                         <TableRow>
                             <TableCell>Image</TableCell>
-                            <TableCell align="right">Name</TableCell>
+                            <TableCell align="right" onClick={()=> this.sortByName()}>Name</TableCell>
                             <TableCell align="right">Phone Number</TableCell>
                             <TableCell align="right">Email</TableCell>
                             <TableCell align="right">DoB</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {this.state.users.map((user) => (
-            <TableRow key={user.name}>
-              <TableCell component="th" scope="row">
-                <img src = {user.picture.thumbnail}></img>
-              </TableCell>
-              <TableCell align="right">{user.name.first} {user.name.last}</TableCell>
-              <TableCell align="right">{user.phone}</TableCell>
-              <TableCell align="right">{user.email}</TableCell>
-              <TableCell align="right">{user.dob.date}</TableCell>
-            </TableRow>
-          ))}
+                        {this.state.users.filter(user => user.name.first.toLowerCase().includes(this.state.search.toLowerCase()) || user.name.last.toLowerCase().includes(this.state.search.toLowerCase())).map((user) => (
+                            <TableRow key={user.name}>
+                                <TableCell component="th" scope="row">
+                                    <img src={user.picture.thumbnail}></img>
+                                </TableCell>
+                                <TableCell align="right">{user.name.first} {user.name.last}</TableCell>
+                                <TableCell align="right">{user.phone}</TableCell>
+                                <TableCell align="right">{user.email}</TableCell>
+                                <TableCell align="right">{user.dob.date}</TableCell>
+                            </TableRow>
+                        ))}
                     </TableBody>
                 </Table>
             </TableContainer>
