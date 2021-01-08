1391c9d01507ba6aef6e71bd4929d9c482ffb00a
joedip16 <joedippolito16@gmail.com>
2021-01-08 08:49:54 -0500

    added search bar to search for employees

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import NavBar from "./components/NavBar"
 import Table from "./components/Table"
+import Form from "./components/Form"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Form />
       <Table />
     </div>
   );
---
+++ src/components/Form.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+
+class Form extends Component {
+  // Setting the component's initial state
+  state = {
+    firstName: "",
+    lastName: ""
+  };
+
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = event.target;
+
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+  };
+
+  render() {
+    // Notice how each input has a `value`, `name`, and `onChange` prop
+    return (
+      <div>
+        <p>
+          Hello {this.state.firstName} {this.state.lastName}
+        </p>
+        <form className="form">
+          <input
+            value={this.state.firstName}
+            name="firstName"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder=""
+          />        
+          <button onClick={this.handleFormSubmit}>Search</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
