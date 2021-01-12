e558607e93339f3388038969a70657aad152ed12
joedip16 <joedippolito16@gmail.com>
2021-01-12 11:03:13 -0500

    deploy react app to git pages with basename prop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import NavBar from "./components/NavBar"
 import Table from "./components/Table"
 import './App.css';
 
 function App() {
   return (
+    <Router basename = "/user_directory">
     <div className="App">
       <NavBar />
       <Table />
     </div>
+    </Router>
   );
 }
 
